c3463ca46b757b2783e3b71c8f9368e1ee176798
landn <landenmick1@gmail.com>
2021-01-28 22:57:48 +0700

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,84 @@
+def find_next_empty(puzzle):
+    # finds the next row, col on the puzzle that's not filled yet --> rep with -1
+    # return row, col tuple (or (None, None) if there is none)
+
+    # keep in mind that we are using 0-8 for our indices
+    for r in range(9):
+        for c in range(9): # range(9) is 0, 1, 2, ... 8
+            if puzzle[r][c] == -1:
+                return r, c
+
+    return None, None  # if no spaces in the puzzle are empty (-1)
+
+def is_valid(puzzle, guess, row, col):
+    # figures out whether the guess at the row/col of the puzzle is a valid guess
+    # returns True or False
+
+    # for a guess to be valid, then we need to follow the sudoku rules
+    # that number must not be repeated in the row, column, or 3x3 square that it appears in
+
+    # let's start with the row
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False # if we've repeated, then our guess is not valid!
+
+    # now the column
+    # col_vals = []
+    # for i in range(9):
+    #     col_vals.append(puzzle[i][col])
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+
+    # and then the square
+    row_start = (row // 3) * 3 # 10 // 3 = 3, 5 // 3 = 1, 1 // 3 = 0
+    col_start = (col // 3) * 3
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+
+    return True
+
+def solve_sudoku(puzzle):
+    # this function will solve our sudoku game using backtracking.
+    
+    row, col = find_next_empty(puzzle)
+
+  
+    if row is None:  
+        return True 
+    
+    
+    for guess in range(1, 10): # range(1, 10) is 1, 2, 3, ... 9
+        
+        if is_valid(puzzle, guess, row, col):
+           
+            puzzle[row][col] = guess
+            
+            if solve_sudoku(puzzle):
+                return True
+        
+        
+        puzzle[row][col] = -1
+
+    #if none of the numbers that we try work, then this puzzle is UNSOLVABLE!!
+    return False
+
+if __name__ == '__main__':
+    example_board = [
+        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
+        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
+        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
+
+        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
+        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
+        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
+
+        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
+        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
+        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
+    ]
+    print(solve_sudoku(example_board))
+    print(example_board)
